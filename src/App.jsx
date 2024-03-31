e614e85bc91f25b4cfa8f8f4928d1f2885864441
DESKTOP-24GU9HH\ASUS <lekhaduy0917880045@gmail.com>
2024-03-31 22:24:23 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/login';
+
+const Layout = () => {
+  return (
+    <>
+      Main
+    </>
+  );
+}
+
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <div>404 NOT FOUND</div>,
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
